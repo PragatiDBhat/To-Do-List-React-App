a7f1721b2a73e51268a5d5e3a28268af44187de8
Pragati D Bhat <163662545+PragatiDBhat@users.noreply.github.com>
2024-03-20 19:57:32 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const TodoModel = require('./Models/Todo');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4040;
 
 // CORS middleware setup
 app.use(cors({
