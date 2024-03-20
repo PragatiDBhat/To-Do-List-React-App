d146b8c3655642b3b60ef83fda8f206cc074349e
Pragati D Bhat <163662545+PragatiDBhat@users.noreply.github.com>
2024-03-20 16:49:49 +0530

    Update Home.jsx

--- todolist/src/Home.jsx
+++ todolist/src/Home.jsx
@@ -5,18 +5,18 @@ import axios from 'axios';
 export default function Home() {
     const [todos,setTodos]=useState([])
     useEffect(()=>{
-      axios.get('http://localhost:3001/get')
+      axios.get('https://todolistreactbackend.vercel.app/get')
       .then(result=>setTodos(result.data))
       .catch(err=>console.log(err))
     },[])
 
     const handleEdit=(id)=>{
-      axios.put('http://localhost:3001/update/'+id)
+      axios.put('https://todolistreactbackend.vercel.app/update/'+id)
       .then(result=>{location.reload()})
       .catch(err=>console.log(err))
     }
     const handleDelete=(id)=>{
-      axios.delete('http://localhost:3001/delete/'+id)
+      axios.delete('https://todolistreactbackend.vercel.app/delete/'+id)
       .then(result=>{location.reload()})
       .catch(err=>console.log(err))
     }
