9c2b6a2a9317ac682dbf5c4fc9e888e0e1a3231c
Pragati D Bhat <163662545+PragatiDBhat@users.noreply.github.com>
2024-03-20 19:49:43 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 // CORS middleware setup
 app.use(cors({
-    origin: ['https://to-do-list-react-app-z1bz.onrender.com'],
+    origin: ['https://to-do-list-react-app-rd90.onrender.com'],
     methods: ['POST', 'GET', 'DELETE', 'PUT','HEAD','OPTIONS'],
     credentials: false,
     allowedHeaders: ['Content-Type']
